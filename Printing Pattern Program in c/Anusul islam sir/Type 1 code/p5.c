6ec7db87d149fc52b99048827d96e64008b4a245
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-08-28 13:52:56 +0600

    wah amazing

---
+++ Printing Pattern Program in c/Anusul islam sir/Type 1 code/p5.c
@@ -0,0 +1,26 @@
+/*
+1
+1 0
+1 0 1
+
+*/
+
+
+#include<stdio.h>
+int main(){
+int n,row,col;
+printf("Enter the value of N = ");
+scanf("%d",&n);
+
+for(row=1; row<=n; row++){
+
+    for(col=1; col<=row; col++){
+
+        printf("%c ",64+col);
+    }
+    printf("\n");
+}
+
+return 0;
+}
+
